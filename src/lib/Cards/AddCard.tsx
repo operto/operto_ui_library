2767aca19362847706914c75979643f8dd42c285
Cassy Van <cvan063@mtroyal.ca>
2022-07-13 11:28:44 -0600

    package update

--- src/lib/Cards/AddCard.tsx
+++ src/lib/Cards/AddCard.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import styled from 'styled-components';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
+import { Card } from '@mui/material/';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
