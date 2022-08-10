161249cb57a626a7d9ef0c7a3b0734d7c61fcbe8
Cassy Van <cvan063@mtroyal.ca>
2022-08-10 11:07:36 -0600

    update react and imports

--- src/stories/Cards/SettingCard.stories.tsx
+++ src/stories/Cards/SettingCard.stories.tsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
-import {
-  MainLabel,
-} from 'lib/SmartCards/StyledSmartCard';
+import { MainLabel } from 'lib/SmartCards/StyledSmartCard';
 
-import SettingCard from 'lib/Cards/SettingCard';
+import { SettingCard } from 'lib/Cards/SettingCard';
 
 export default {
-    title: 'Operto/Cards/Setting Card',
-    mode: 'regular',
-    component: SettingCard,
-  
-  } as ComponentMeta<typeof SettingCard>;
+  title: 'Operto/Cards/Setting Card',
+  mode: 'regular',
+  component: SettingCard,
+} as ComponentMeta<typeof SettingCard>;
 
+const Template: ComponentStory<typeof SettingCard> = args => (
+  <ThemeProvider theme={{ ...args }}>
+    <SettingCard {...args}>
+      <MainLabel>Empty Content</MainLabel>
+    </SettingCard>
+  </ThemeProvider>
+);
 
-  const Template: ComponentStory<typeof SettingCard> = (args) => (
-    <ThemeProvider theme={{ ...args }}>
-        <SettingCard {...args}>
-            <MainLabel>Empty Content</MainLabel>
-        </SettingCard>
-    </ThemeProvider>
-
-  );
-
-  export const Basic = Template.bind({});
+export const Basic = Template.bind({});
 Basic.args = {
   title: 'Setting Card',
 };
-
