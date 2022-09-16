92d496f19f3950b31201629c3a172d292a8d69e7
judychern <chern.judy@gmail.com>
2022-09-16 09:38:37 -0700

    pushing solid borderr

---
+++ lib/components/TableActionbar/TableActionBar.d.ts
@@ -0,0 +1,11 @@
+interface TableActionBarProps {
+    endSection?: any;
+    startSection?: any;
+    showSecondary?: boolean;
+    secondaryContent?: any;
+    fullWidth?: boolean;
+    isMobile: boolean;
+}
+export declare const TableActionBar: ({ endSection, startSection, showSecondary, secondaryContent, fullWidth, isMobile, }: TableActionBarProps) => JSX.Element;
+export declare const Base: import("styled-components").StyledComponent<"div", any, {}, never>;
+export {};
